71d1dd2fb42b743e8af5cab986aa36c26fa03e24
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2025-04-23 22:42:39 -0300

    update

--- packages/lib/src/index/createHttpClient/index.ts
+++ packages/lib/src/index/createHttpClient/index.ts
@@ -3,3 +3,11 @@ import axios from 'axios';
 export const createHttpClient = () => {
   return axios.create();
 };
+
+/*
+## It's mandatory to have support for circuit-breaker and cache
+- If two components are doing a request for the same place, we should not do the request twice considering a stale time
+
+- https://github.com/barnendu/axios-breaker
+- https://www.npmjs.com/package/axios-cache-adapter
+*/
