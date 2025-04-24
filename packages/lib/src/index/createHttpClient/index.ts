e69b3823713efd76176967e6ef852435182bf594
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2025-04-24 00:10:00 -0300

    update

--- packages/lib/src/index/createHttpClient/index.ts
+++ packages/lib/src/index/createHttpClient/index.ts
@@ -10,4 +10,28 @@ export const createHttpClient = () => {
 
 - https://github.com/barnendu/axios-breaker
 - https://www.npmjs.com/package/axios-cache-adapter
+
+
+HttpClient.request({
+  method: "GET",
+  url: "https://api.github.com/users/omariosouto", // bookmarks[method]["https://api.github.com/users/:username"]
+  headers: {
+    "Content-Type": "application/json",
+  },
+  // Cache Config -> https://tanstack.com/query/v4/docs/framework/react/guides/important-defaults
+  staleTime: "5m", // avoid deduplicated requests
+  // Circuit Breaker Config -> https://github.com/barnendu/axios-breaker
+  retry: 3 // by default 3 with exponential backoff of 100ms for first, 200ms for second and 400ms for third
+  // -> statusCodesToRetry: [[100, 199], [429, 429], [500, 599]],
+  // options: { //customizaable optionsfor circuit breaker
+  //   failureThreshold: 3, // maximum no of failed before circuit break,
+  //   successThreshold: 2, // maximum success call to close the circuit
+  //   timeout: 5000, // reset timeout for next call while in open state
+  //   fallbackPolicy: callback // fallbackpolicy if any while circuit is open. For example you want to  divert the api call to different endpoint.
+  // }
+})
+
+// TODO: Add to common-core
+import ms from 'ms';
+
 */
