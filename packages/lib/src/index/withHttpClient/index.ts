ebc0fed7ab0110837fa2e346307e75eda3ab92b4
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2025-05-03 21:29:10 -0300

    update

--- packages/lib/src/index/withHttpClient/index.ts
+++ packages/lib/src/index/withHttpClient/index.ts
@@ -3,8 +3,8 @@ import { bookmarkMock } from "../bookmarkMock";
 export function withHttpClient(Component: any) {
   return async function WithHttpClient(props: any) {
     const searchParams = await props.searchParams;
-    if(searchParams.http_state) {
-      bookmarkMock.set(JSON.parse(atob(searchParams.http_state)));
+    if(searchParams.httpclient_state) {
+      bookmarkMock.set(JSON.parse(atob(searchParams.httpclient_state)));
     }
     const output = await Component(props);
     return output;
